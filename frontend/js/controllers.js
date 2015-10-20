cb398e0ca6100db3cf98c251d3df8bd1e6f672e6
taurenk <tauren.kristich@gmail.com>
2015-10-19 21:53:22 -0400

    *feat(services):* USGS Earthquake api added *feat(controllers):* markers are now displayed on map

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -1,25 +1,38 @@
 
 var appControllers = angular.module('appControllers', []);
 
-appControllers.controller('mapController', function($scope, RSOEApi, geolocation){
+appControllers.controller('mapController', function($scope, USGSApi, geolocation){
+
+    $scope.markers = [];
+    $scope.options = {scrollwheel: false};
 
     geolocation.getLocation()
         .then(function(data){
             $scope.coords = {lat:data.coords.latitude, long:data.coords.longitude};
-            $scope.map = {center: {latitude: data.coords.latitude, longitude: data.coords.longitude }, zoom: 14 };
+            $scope.map = {center: {latitude: data.coords.latitude, longitude: data.coords.longitude }, zoom: 10 };
         })
         .catch(function(response) {
-             $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 14 };
+             $scope.map = {center: {latitude: 51.219053, longitude: 4.404418 }, zoom: 1 };
         })
 
-    $scope.options = {scrollwheel: false};
+    $scope.onClick = function(model) {
+        model.show = !model.show;
+    };
 
-    $scope.testData = RSOEApi.getEarthquakes()
+    // Set Markers
+    $scope.markers = [];
+    var marker1 = {id: 1, latitude: 37.769, longitude: -122.44, title:'Test Marker!', content: 'content. ', show: false};
+    $scope.markers.push(marker1);
+
+
+
+
+    $scope.testData = USGSApi.getEarthquakes()
         .success(function (response) {
-            //alert('Pass: '+ response);
+            $scope.testData = response;
         })
         .error(function (response) {
-            //alert('Fail: ' + response);
+            $scope.testData = response;
         });
 
 
--- frontend/js/services.js
+++ frontend/js/services.js
@@ -1,13 +1,13 @@
 
 var appServices = angular.module('appServices', []);
 
-appServices.factory('RSOEApi', ['$http', function ($http) {
+appServices.factory('USGSApi', ['$http', function ($http) {
 
-    baseUrl = 'https://hisz.rsoe.hu/ws/';
+    baseUrl = 'http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson';
 
     return {
         getEarthquakes: function () {
-            return $http.get(baseUrl + '' );
+            return $http.get(baseUrl + '&starttime=2015-10-19&endtime=2015-10-19' );
         }
     }
 
